30d5928f386c169cae7487d21c4042bdcbcbbcc5
Ryan Tanay <ryantanay@newevolution.org>
2016-01-25 15:18:05 -0600

    Wrote tests to illustrate template strings are still strings, they can evaluate variables & expressions & execute functions, and be multi-line

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -9,6 +9,29 @@ describe("Test suite verification", function() {
 describe("Template strings", function() {
   it("Should be a string", function() {
     var string = `abc`;
-    expect(string).to.be.a.("string");
+    expect(string).to.be.a("string");
+  });
+
+  it("Can evaluate variables", function() {
+    var bar = "baz";
+    var string = `foo${bar}`;
+    expect(string).to.equal("foobaz");
+  });
+
+  it("Can execute functions", function() {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal("The date is: " + date);
+  });
+
+  it("Can evaluate expressions", function() {
+    var string = `${3*7}`;
+    expect(string).to.equal("21");
+  });
+
+  it("Can be multi-line", function() {
+    var string = `Line one,
+Line two`;
+    expect(string).to.equal("Line one,\nLine two");
   });
 });
