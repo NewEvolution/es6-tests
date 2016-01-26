555e57babdfd9b1c4ba53f5b287c071642c37588
Ryan Tanay <ryantanay@newevolution.org>
2016-01-26 09:32:37 -0600

    Wrote tests for exploring destructuring after wrangling with the object syntax for a bit.

--- tests/destructuring.js
+++ tests/destructuring.js
@@ -1,1 +1,17 @@
 var expect = require("chai").expect;
+
+describe("Destructuring", function() {
+  it("Can assign variables via arrays", function() {
+    var [first, , last] = [1, 2, 3];
+    expect(first).to.equal(1);
+    expect(last).to.equal(3);
+  });
+
+  it("Can assign variables via objects, deeply", function() {
+    var foo = {first:1, second:{deep:2}, third:3};
+    var {first:a, second: {deep:b}, third:c} = foo;
+    expect(a).to.equal(1);
+    expect(b).to.equal(2);
+    expect(c).to.equal(3);
+  });
+});
